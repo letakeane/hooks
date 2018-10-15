83f68961b1cf7a5ece75188ff5ebfc7d79042bbe
Leta Keane <letakeane+turing@gmail.com>
2018-10-15 10:15:03 -0600

    Add currying function

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const add2 = number => number + 2;
-const add3 = number => number + 3;
+const addNum = num => {
+  const add = number => number + num;
+
+  return add;
+}
+
+const add2 = addNum(2);
+const add3 = addNum(3);
 
 module.exports = { add2, add3 };
